90be4d21820812e36bffa1b18e70c5e2db494357
Janith <janithr@saveenergysystems.com>
2026-05-12 14:09:59 +0530

    add pino setup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get,Logger  } from '@nestjs/common';
 
 @Controller()
 export class AppController {
+  private readonly logger = new Logger(AppController.name);
   @Get()
   getHello(): string {
     return 'Hello from NestJS Railway Observability!';
@@ -17,17 +18,20 @@ export class AppController {
 
   @Get('health/live')
   healthLive() {
+    this.logger.debug('This is a massive error that OTel will catch!');
     return { status: 'alive' };
   }
 
   @Get('health/slow')
   async healthSlow() {
     await new Promise(resolve => setTimeout(resolve, 2000));
+    this.logger.warn('This is a massive warn that OTel will catch!');
     return { status: 'slow', delay: '2s' };
   }
 
   @Get('health/error')
   healthError() {
+    this.logger.error('This is a massive error that OTel will catch!');
     throw new Error('Test error for observability!');
   }
 }
