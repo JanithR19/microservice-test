e28a554d66562becc7cf8cd6358ad222788da7cf
Janith <janithr@saveenergysystems.com>
2026-05-13 10:41:14 +0530

    add sentry config to recieve error logs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get,Logger  } from '@nestjs/common';
+import * as Sentry from "@sentry/nestjs";
 
 @Controller()
 export class AppController {
@@ -19,12 +20,14 @@ export class AppController {
   @Get('health/live')
   healthLive() {
     this.logger.debug('This is a massive error that OTel will catch!');
+    Sentry.logger.info('User triggered test log', { action: 'test_log' })
     return { status: 'alive' };
   }
 
   @Get('health/slow')
   async healthSlow() {
     await new Promise(resolve => setTimeout(resolve, 2000));
+    Sentry.logger.warn('User triggered test warn log', { action: 'test_log' })
     this.logger.warn('This is a massive warn that OTel will catch!');
     return { status: 'slow', delay: '2s' };
   }
@@ -32,6 +35,7 @@ export class AppController {
   @Get('health/error')
   healthError() {
     this.logger.error('This is a massive error that OTel will catch!');
+    Sentry.logger.error('User triggered test error log', { action: 'test_log' })
     throw new Error('Test error for observability!');
   }
 }
--- src/instrument.ts
+++ src/instrument.ts
@@ -20,7 +20,8 @@ Sentry.init({
   dsn: process.env.SENTRY_DSN, // Add this to your Railway Variables
   integrations: [
     nodeProfilingIntegration(),
-  ],
+    Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
+  ],enableLogs:true,
   // Performance Monitoring
   tracesSampleRate: 1.0, // Capture 100% of the transactions
   // Set sampling rate for profiling - this is relative to tracesSampleRate
